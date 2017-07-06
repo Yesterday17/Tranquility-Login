d842c2038069cb9c4f00cd7409db7ba7ccb1182a
Haozhe Jiang <t@yesterday17.cn>
2017-07-06 11:14:26 +0800

    优化设计 下载客户端有提示状态

---
+++ Tranquility Login/DownloadForm.cs
@@ -0,0 +1,94 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tranquility_Login
+{
+    public partial class DownloadForm : Form
+    {
+        public static String git_repository = "https://git.coding.net/yesterday17/TestMinecraft.git";
+        private static Form frmDownload;
+        private static Label label_download;
+        private Boolean clone_finished
+        {
+            get
+            {
+                return false;
+            }
+            set
+            {
+                System.Environment.Exit(0);
+            }
+        }
+
+        delegate void textTip(double receive, double total);
+
+        public DownloadForm()
+        {
+            init();            
+        }
+
+        public DownloadForm(String text)
+        {
+            init();
+            this.Text = text;
+        }
+
+        private void init()
+        {
+            InitializeComponent();
+            frmDownload = this;
+            label_download = this.lb_download;
+        }
+
+        public static bool TransferProgress(TransferProgress progress)
+        {
+            //MessageBox.Show($"Objects: {progress.ReceivedObjects} of {progress.TotalObjects}, Bytes: {progress.ReceivedBytes}");
+
+            textTip textEdit = delegate (double receive, double total)
+            {
+                label_download.Text = (receive / total * 100).ToString("F2") + "%";
+            };
+            label_download.Invoke(textEdit, progress.ReceivedObjects, progress.TotalObjects);
+
+            //label_download.Text = (progress.ReceivedObjects/progress.TotalObjects).ToString("#0.00") + "%";
+            return true;
+        }
+
+        private void DownloadForm_Load(object sender, EventArgs e)
+        {
+            Task clone = new Task(CloneMethod);
+            clone.Start();
+        }
+
+        private void CloneMethod()
+        {
+            try
+            {
+                CloneOptions co = new CloneOptions
+                {
+                    BranchName = "master",
+                    OnTransferProgress = DownloadForm.TransferProgress,
+                    CredentialsProvider = (_url, _user, _cred) => new UsernamePasswordCredentials { Username = "yesterday17", Password = "001206" }
+                };
+
+                Repository.Clone(git_repository, Application.StartupPath + "/minecraft/", co);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                MessageBox.Show("请尝试重写启动该程序！");
+                System.Environment.Exit(0);
+            }
+            MessageBox.Show("下载完成！");
+            clone_finished = true;
+        }
+    }
+}
--- Tranquility Login/Program.cs
+++ Tranquility Login/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibGit2Sharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,6 +7,137 @@ using System.Windows.Forms;
 
 namespace Tranquility_Login
 {
+    class MainJudge
+    {
+        public Repository repo;
+        public Branch latest = null;
+        public Branch master = null;
+        public Branch origin_latest = null;
+        public Branch origin_master = null;
+
+        public static string path = Application.StartupPath + "/minecraft/";
+
+        public enum LoadState
+        {
+            startup = 1,
+            exit = 2,
+
+            init = 3,
+            update = 4
+        };
+        private LoadState state;
+
+        public MainJudge(String arg)
+        {
+            switch (arg)
+            {
+                case "startup":
+                case "login":
+                    state = LoadState.startup;
+                    break;
+
+                case "exit":
+                    state = LoadState.exit;
+                    break;
+
+                case "init":
+                    state = LoadState.startup;
+                    break;
+
+                case "update":
+                case "upgrade":
+                    state = LoadState.update;
+                    break;
+
+                default:
+                    state = LoadState.init;
+                    break;
+            }
+        }
+
+        public MainJudge()
+        {
+            state = LoadState.init;
+        }
+
+        public Boolean mcRepositoryIsValid(String path)
+        {
+            if (!Repository.IsValid(path))
+                return false;
+
+            using (var repo = new Repository(path))
+            {
+                foreach (Branch b in repo.Branches)
+                {
+                    switch (b.FriendlyName)
+                    {
+                        case "latest":
+                            this.latest = b;
+                            break;
+
+                        case "master":
+                            this.master = b;
+                            break;
+
+                        case "origin/latest":
+                            this.origin_latest = b;
+                            break;
+
+                        case "origin/master":
+                            this.origin_master = b;
+                            break;
+                    }
+                    MessageBox.Show(b.FriendlyName);
+                }
+            }
+
+            if (this.origin_latest == null || this.origin_master == null)
+                return false;
+
+            return true;
+        }
+
+        public void Load()
+        {
+            Boolean valid = mcRepositoryIsValid(path);
+
+            if (valid)
+            {
+                switch (state)
+                {
+                    case LoadState.startup:
+                        repo = new Repository(path);
+                        //$ git checkout .
+                        Commands.Checkout(repo, master);
+                        //$ git checkout latest
+                        Commands.Checkout(repo, latest);
+                        break;
+
+                    case LoadState.exit:
+                        repo = new Repository(path);
+                        //$ git checkout .
+                        Commands.Checkout(repo, latest);
+                        //$ git checkout master
+                        Commands.Checkout(repo, master);
+                        break;
+
+                    case LoadState.init:
+                        MessageBox.Show("工作目录已正常配置！请使用MultiMC启动！");
+                        System.Environment.Exit(0);
+                        break;
+
+                    case LoadState.update:
+                        //
+                        break;
+                }
+            }
+            else
+            {
+                Application.Run(new DownloadForm("客户端下载中……"));
+            }
+        }
+    }
+
     static class Program
     {
         /// <summary>
@@ -14,12 +146,21 @@ namespace Tranquility_Login
         [STAThread]
         static void Main(string[] args)
         {
+            MainJudge main;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
+            MessageBox.Show(args.Length.ToString());
+
             if (args.Length == 1)
-                Application.Run(new Main(args[0]));
-            else Application.Run(new Main());
+                main = new MainJudge(args[0]);
+            else
+                main = new MainJudge();
+
+            main.Load();
+            MessageBox.Show("exit");
         }
+        
     }
 }
