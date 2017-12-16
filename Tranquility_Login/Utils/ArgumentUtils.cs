0577e9137ddf47179a15708873ba7142f15d3400
Yesterday17 <t@yesterday17.cn>
2017-12-16 20:11:10 +0800

    Add LICENSE; Start Argument part.

---
+++ Tranquility_Login/Utils/ArgumentUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tranquility_Login.Utils
+{
+    public static class ArgumentUtils
+    {
+        /* 
+         * 此处简单概括一下参数的使用方法。
+         * 1.若没有参数
+         * 
+         * 
+         */
+
+        public static Boolean init = false;
+
+    }
+}
